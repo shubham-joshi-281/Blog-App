a8d3f281121fb41f4bff8478b50c3a64830448c4
Shubham <shubhamjoshi281@gmail.com>
2023-08-14 15:39:10 +0530

    file updated

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/blog", blogRouter);
 
-app.get("*", (req, res) => {
+app.use("/", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 // port at listen
